b195b0d9bdb1a8cbd2cb67f0d0923919fac0e9f0
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-02 03:43:44 +0800

    Activate Dishes and Recipes routes

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet')
 const logger = require('morgan')
 
 // Import resource routes
+const dishRoutes = require('../routes/dishRoutes')
+const recipeRoutes = require('../routes/recipeRoutes')
 
 // Instantiate server
 const server = express()
@@ -13,6 +15,8 @@ server.use(express.json())
 server.use(logger('dev'))
 
 // Activate routes
+server.use('/api/dishes', dishRoutes)
+server.use('/api/recipes', recipeRoutes)
 server.use('/', (req, res) => {
   res.send(`<h1>Welcome to the Recipe Book API</h1>`)
 })
