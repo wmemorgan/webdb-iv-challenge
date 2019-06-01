b79b80eaef1c70e1d728d2220d99a48848096b11
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-01 17:24:43 +0800

    Seed UnitsOfMeasure table

---
+++ data/seeds/001-units.js
@@ -0,0 +1,20 @@
+exports.seed = async function (knex) {
+  // Inserts seed entries
+  await knex('UnitsOfMeasure').insert([
+    { unit: 'teaspoon', abbreviation: 'tsp'},
+    { unit: 'tablespoon', abbreviation: 'tbsp' },
+    { unit: 'fluid_ounce', abbreviation: 'fl.oz' },
+    { unit: 'cup', abbreviation: 'C' },
+    { unit: 'pint', abbreviation: 'pt' },
+    { unit: 'quart', abbreviation: 'qt' },
+    { unit: 'gallon', abbreviation: 'gal' },
+    { unit: 'ounce', abbreviation: 'oz' },
+    { unit: 'pound', abbreviation: 'lb' },
+    { unit: 'milliliter', abbreviation: 'mL'},
+    { unit: 'litres', abbreviation: 'L' },
+    { unit: 'grams', abbreviation: 'g' },
+    { unit: 'kilograms', abbreviation: 'kg' },
+    { unit: 'Celcius', abbreviation: '°C' },
+    { unit: 'Fahrenheit', abbreviation: '°F' },
+  ]);
+};
